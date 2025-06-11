418acb43c6fcb264abe8ba278d7ef2630fa8c489
Shresht-Ahuja <152013182+Shresht-Ahuja@users.noreply.github.com>
2025-06-11 09:56:06 +0530

    added simple web-scraping

--- roadmap.py
+++ roadmap.py
@@ -1,4 +1,7 @@
 from duckduckgo_search import DDGS
+import requests
+from bs4 import BeautifulSoup
+
 
 def get_learning_links(skill, num_results=5):
     query = f"{skill} tutorial site:geeksforgeeks.org OR site:youtube.com OR site:https://www.w3schools.com"
@@ -16,6 +19,10 @@ def get_learning_links(skill, num_results=5):
 if __name__ == "__main__":
     user_input = input("Enter a skill or role you want to learn: ")
     learning_links = get_learning_links(user_input)
+    response = requests.get(f"https://www.w3schools.com/whatis/whatis_{user_input}.asp")
+    soup = BeautifulSoup(response.text, 'html.parser')
+    t = soup.find("title").text
+    print(t)
     
     print("\nTop Learning Resources:\n")
     for i, (title, url) in enumerate(learning_links, 1):
